6508c8671d16e82a7a0bb34924e6b39cd5747fb3
Kenta OONO <oono@preferred.jp>
2015-07-24 17:28:16 +0900

    Refresh emacs.d

---
+++ .emacs.d/inits/00-keybind.el
@@ -0,0 +1,8 @@
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8) 
+
+(show-paren-mode t)
+
+(global-set-key "\C-h" 'delete-backward-char)
---
+++ .emacs.d/inits/20-package.el
@@ -0,0 +1,26 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    php-mode
+    scala-mode
+    markdown-mode
+    scss-mode
+    haskell-mode
+    google-c-style
+    yaml-mode
+    open-junk-file
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
